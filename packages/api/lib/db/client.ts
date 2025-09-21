64e7f21f32d4ffd7f93465e04530008db4d1a2f4
Spandan Barve <spandan567@gmail.com>
2025-09-21 21:20:23 +0800

    add database client setup with drizzle-orm and bun-sqlite

---
+++ packages/api/lib/db/client.ts
@@ -0,0 +1,19 @@
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
+import schema from "./schema";
+import { eq } from "drizzle-orm";
+import { SQL } from "bun";
+
+const sqliteFile = process.env.SQLITE_FILE || "./filosign.db";
+
+const sqlite = new Database(sqliteFile);
+sqlite.run("PRAGMA foreign_keys = ON");
+
+const dbClient = drizzle({
+  client: sqlite,
+  schema: schema,
+  casing: "snake_case",
+});
+
+export default dbClient;
+export const sql = new SQL(sqliteFile);
