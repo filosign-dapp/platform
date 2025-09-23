54f6cc4d2565ecc3640f1cb4e61b4850b2db7f65
Spandan Barve <spandan567@gmail.com>
2025-09-23 12:52:36 +0800

    add user and profile tables to schema

---
+++ packages/api/lib/db/schema/user.ts
@@ -0,0 +1,24 @@
+import * as t from "drizzle-orm/sqlite-core";
+import { tEvmAddress, tJsonString, timestamps } from "../helpers";
+
+export const users = t.sqliteTable("users", {
+  walletAddress: tEvmAddress().primaryKey(),
+  email: t.text(),
+  lastActiveAt: t.integer().notNull(),
+  keygenDataJson: tJsonString(),
+
+  ...timestamps,
+});
+
+export const profiles = t.sqliteTable("profiles", {
+  walletAddress: tEvmAddress()
+    .references(() => users.walletAddress, {
+      onDelete: "cascade",
+    })
+    .primaryKey(),
+  username: t.text().notNull().unique(),
+  displayName: t.text().notNull(),
+  avatarUrl: t.text(),
+  bio: t.text().default(""),
+  metadataJson: tJsonString().default({ value: "{}" }),
+});
