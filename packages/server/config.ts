575877c9a62b2f9a535cb9b19cc944aab13e1323
Spandan Barve <spandan567@gmail.com>
2025-09-24 04:06:36 +0800

    default indexer config

---
+++ packages/server/config.ts
@@ -0,0 +1,13 @@
+const INDEXER = {
+  CONFIRMATIONS: 6n,
+  MAX_BATCH_BLOCKS: 200n,
+  POLL_INTERVAL_MS: 3_000,
+  LOGS_PER_TX: 10,
+  DEFAULT_START_BLOCK: 3_044_275n,
+};
+
+const config = {
+  INDEXER,
+};
+
+export default config;
