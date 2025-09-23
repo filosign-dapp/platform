d101b780a330cff6d5d63acbe647a66fe5710e3b
Spandan Barve <spandan567@gmail.com>
2025-09-23 21:30:21 +0800

    add pubkey

---
+++ packages/server/lib/db/schema/user.ts
@@ -0,0 +1,25 @@
+import * as t from "drizzle-orm/sqlite-core";
+import { tEvmAddress, tJsonString, timestamps } from "../helpers";
+
+export const users = t.sqliteTable("users", {
+  walletAddress: tEvmAddress().primaryKey(),
+  email: t.text(),
+  lastActiveAt: t.integer().notNull(),
+  keygenDataJson: tJsonString(),
+  encryptionPublicKey: t.text(),
+
+  ...timestamps,
+});
+
+export const profiles = t.sqliteTable("profiles", {
+  walletAddress: tEvmAddress()
+    .references(() => users.walletAddress, {
+      onDelete: "cascade",
+    })
+    .primaryKey(),
+  username: t.text().notNull().unique(),
+  displayName: t.text().notNull(),
+  avatarUrl: t.text(),
+  bio: t.text().default(""),
+  metadataJson: tJsonString().default({ value: "{}" }),
+});
