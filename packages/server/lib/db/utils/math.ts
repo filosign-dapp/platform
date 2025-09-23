1bea91bcf8435cc9a0665ba8b442ab0b3d3e4785
Spandan Barve <spandan567@gmail.com>
2025-09-24 04:07:23 +0800

    add utility functions for bigint min and max calculations

---
+++ packages/server/lib/db/utils/math.ts
@@ -0,0 +1,7 @@
+export function bigIntMin(...values: bigint[]): bigint {
+  return values.reduce((min, v) => (v < min ? v : min));
+}
+
+export function bigIntMax(...values: bigint[]): bigint {
+  return values.reduce((max, v) => (v > max ? v : max));
+}
