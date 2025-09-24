16319842f7b1f2a9683fa97fb7003bcf606f52c2
Spandan Barve <spandan567@gmail.com>
2025-09-25 06:16:26 +0800

    graceful db handling

--- packages/server/lib/indexer/worker.ts
+++ packages/server/lib/indexer/worker.ts
@@ -28,205 +28,238 @@ export type TypedWorker = Omit<Worker, "postMessage" | "addEventListener"> & {
 
 addEventListener("message", (ev: MessageEvent<Incoming>) => {
   const job = ev.data;
-  processJob(job).catch((error) => {
-    const res: Outgoing = {
-      id: job.id,
-      result: null,
-      error: error,
-    };
-    postMessage(res);
-  });
+  const db = createDbClient();
+
+  if (job.lockedUntil && job.lockedUntil > Date.now()) {
+    setTimeout(() => {
+      db.$client.close();
+    }, job.lockedUntil - Date.now());
+  }
+
+  processJob(job, db)
+    .then((result) => {
+      const res: Outgoing = {
+        id: job.id,
+        result: result,
+        error: null,
+      };
+      postMessage(res);
+    })
+    .catch((error) => {
+      const errMsg = error instanceof Error ? error.message : String(error);
+      const res: Outgoing = {
+        id: job.id,
+        result: null,
+        error: errMsg,
+      };
+      postMessage(res);
+    });
 });
 
-async function processJob(job: Job) {
-  const db = createDbClient();
-  const contracts = getContracts(getProvider());
-  // @ts-ignore This is th best we can do for now
-  const log: ProviderLogEntry = job.payload;
+async function processJob(job: Job, dbC: ReturnType<typeof createDbClient>) {
+  try {
+    dbC.transaction(async (db) => {
+      const contracts = getContracts(getProvider());
+      // @ts-ignore This is th best we can do for now
+      const log: ProviderLogEntry = job.payload;
 
-  const jobTypeParts = job.type.split("_");
+      const jobTypeParts = job.type.split("_");
 
-  if (jobTypeParts[0] === "EVENT") {
-    const contractName = jobTypeParts[1];
+      if (jobTypeParts[0] === "EVENT") {
+        const contractName = jobTypeParts[1];
 
-    if (contractName === "FSManager") {
-      if (log.eventName === "SenderApproved") {
-        db.update(schema.shareRequests)
-          .set({ status: "ACCEPTED" })
-          .where(
-            and(
-              eq(schema.shareRequests.senderWallet, log.args.sender),
-              eq(schema.shareRequests.recipientWallet, log.args.recipient),
-              eq(schema.shareRequests.status, "PENDING")
-            )
-          );
+        if (contractName === "FSManager") {
+          if (log.eventName === "SenderApproved") {
+            db.update(schema.shareRequests)
+              .set({ status: "ACCEPTED" })
+              .where(
+                and(
+                  eq(schema.shareRequests.senderWallet, log.args.sender),
+                  eq(schema.shareRequests.recipientWallet, log.args.recipient),
+                  eq(schema.shareRequests.status, "PENDING")
+                )
+              )
+              .run();
 
-        const approval = db
-          .insert(schema.shareApprovals)
-          .values({
-            senderWallet: log.args.sender,
-            recipientWallet: log.args.recipient,
-            active: true,
-          })
-          .onConflictDoUpdate({
-            target: [
-              schema.shareApprovals.senderWallet,
-              schema.shareApprovals.recipientWallet,
-            ],
-            set: {
-              active: true,
-            },
-          })
-          .returning()
-          .get();
+            const approval = db
+              .insert(schema.shareApprovals)
+              .values({
+                senderWallet: log.args.sender,
+                recipientWallet: log.args.recipient,
+                active: true,
+              })
+              .onConflictDoUpdate({
+                target: [
+                  schema.shareApprovals.senderWallet,
+                  schema.shareApprovals.recipientWallet,
+                ],
+                set: {
+                  active: true,
+                },
+              })
+              .returning()
+              .get();
 
-        db.insert(schema.shareApprovalHistory)
-          .values({
-            approvalId: approval.id,
-            action: "ENABLED",
-            blockNumber: log.blockNumber,
-            txHash: log.transactionHash,
-          })
-          .run();
-      }
+            if (!approval || !approval.id) {
+              throw new Error("Fail to create / retrive approval row");
+            }
 
-      if (log.eventName === "SenderRevoked") {
-        const approval = db
-          .update(schema.shareApprovals)
-          .set({
-            active: false,
-          })
-          .where(
-            and(
-              eq(schema.shareApprovals.senderWallet, log.args.sender),
-              eq(schema.shareApprovals.recipientWallet, log.args.recipient)
-            )
-          )
-          .returning()
-          .get();
+            db.insert(schema.shareApprovalHistory)
+              .values({
+                approvalId: approval.id,
+                action: "ENABLED",
+                blockNumber: log.blockNumber,
+                txHash: log.transactionHash,
+              })
+              .run();
+          }
 
-        db.insert(schema.shareApprovalHistory)
-          .values({
-            approvalId: approval.id,
-            action: "REVOKED",
-            blockNumber: log.blockNumber,
-            txHash: log.transactionHash,
-          })
-          .run();
-      }
-    }
+          if (log.eventName === "SenderRevoked") {
+            const approval = db
+              .update(schema.shareApprovals)
+              .set({
+                active: false,
+              })
+              .where(
+                and(
+                  eq(schema.shareApprovals.senderWallet, log.args.sender),
+                  eq(schema.shareApprovals.recipientWallet, log.args.recipient)
+                )
+              )
+              .returning()
+              .get();
 
-    if (contractName === "FSFileRegistry") {
-      if (log.eventName === "FileRegistered") {
-        const fileData = await contracts.FSFileRegistry.read.getFileData([
-          log.args.cidIdentifier,
-        ]);
-
-        if (!fileData.pieceCidPrefix) {
-          throw new Error(
-            "No pieceCidPrefix in file data, invalid event maybe?"
-          );
-        }
-
-        const cid = concatHex([
-          fileData.pieceCidPrefix,
-          toHex(fileData.pieceCidTail),
-        ]);
-
-        db.update(schema.files)
-          .set({
-            onchainTxHash: log.transactionHash,
-          })
-          .where(eq(schema.files.pieceCid, cid))
-          .run();
-      }
-
-      if (log.eventName === "FileAcknowledged") {
-        const fileData = await contracts.FSFileRegistry.read.getFileData([
-          log.args.cidIdentifier,
-        ]);
-
-        if (!fileData.pieceCidPrefix) {
-          throw new Error(
-            "No pieceCidPrefix in file data, invalid event maybe?"
-          );
-        }
-
-        const cid = concatHex([
-          fileData.pieceCidPrefix,
-          toHex(fileData.pieceCidTail),
-        ]);
-
-        const file = db
-          .select()
-          .from(schema.files)
-          .where(eq(schema.files.pieceCid, cid))
-          .get();
-
-        if (!file) {
-          throw new Error("File not found for FileAcknowledged event");
-        }
-
-        if (file.acknowledged) {
-          if (file.acknowledgedTxHash === log.transactionHash) {
-            return;
-          } else {
-            throw new Error(
-              "Panic! File acknowledged yet event emitted again!"
-            );
+            if (approval && approval.id) {
+              db.insert(schema.shareApprovalHistory)
+                .values({
+                  approvalId: approval.id,
+                  action: "REVOKED",
+                  blockNumber: log.blockNumber,
+                  txHash: log.transactionHash,
+                })
+                .run();
+            }
           }
         }
 
-        db.update(schema.files)
-          .set({
-            acknowledged: true,
-            acknowledgedTxHash: log.transactionHash,
-          })
-          .where(eq(schema.files.pieceCid, cid));
-      }
+        if (contractName === "FSFileRegistry") {
+          if (log.eventName === "FileRegistered") {
+            const fileData = await contracts.FSFileRegistry.read.getFileData([
+              log.args.cidIdentifier,
+            ]);
 
-      if (log.eventName === "SignatureSubmitted") {
-        const signatureData =
-          await contracts.FSFileRegistry.read.getSignatureData([
-            log.args.cidIdentifier,
-          ]);
+            if (!fileData.pieceCidPrefix) {
+              throw new Error(
+                "No pieceCidPrefix in file data, invalid event maybe?"
+              );
+            }
 
-        const fileData = await contracts.FSFileRegistry.read.getFileData([
-          log.args.cidIdentifier,
-        ]);
+            const cid = concatHex([
+              fileData.pieceCidPrefix,
+              toHex(fileData.pieceCidTail),
+            ]);
 
-        const cid = concatHex([
-          fileData.pieceCidPrefix,
-          toHex(fileData.pieceCidTail),
-        ]);
-
-        const signatureExists = db
-          .select()
-          .from(schema.fileSignatures)
-          .where(eq(schema.fileSignatures.onchainTxHash, log.transactionHash))
-          .get();
-
-        if (signatureExists) {
-          return;
-        }
-
-        db.insert(schema.fileSignatures)
-          .values({
-            filePieceCid: cid,
-            signerWallet: log.args.signer,
-            signatureVisualHash: signatureData.signatureVisualHash,
-            timestamp: signatureData.timestamp,
-            compactSignature: serializeCompactSignature(
-              signatureToCompactSignature({
-                v: BigInt(signatureData.v),
-                r: signatureData.r,
-                s: signatureData.s,
+            db.update(schema.files)
+              .set({
+                onchainTxHash: log.transactionHash,
               })
-            ),
-            onchainTxHash: log.transactionHash,
-          })
-          .run();
+              .where(eq(schema.files.pieceCid, cid))
+              .run();
+          }
+
+          if (log.eventName === "FileAcknowledged") {
+            const fileData = await contracts.FSFileRegistry.read.getFileData([
+              log.args.cidIdentifier,
+            ]);
+
+            if (!fileData.pieceCidPrefix) {
+              throw new Error(
+                "No pieceCidPrefix in file data, invalid event maybe?"
+              );
+            }
+
+            const cid = concatHex([
+              fileData.pieceCidPrefix,
+              toHex(fileData.pieceCidTail),
+            ]);
+
+            const file = db
+              .select()
+              .from(schema.files)
+              .where(eq(schema.files.pieceCid, cid))
+              .get();
+
+            if (!file) {
+              throw new Error("File not found for FileAcknowledged event");
+            }
+
+            if (file.acknowledged) {
+              if (file.acknowledgedTxHash === log.transactionHash) {
+                return;
+              } else {
+                throw new Error(
+                  "Panic! File acknowledged yet event emitted again!"
+                );
+              }
+            }
+
+            db.update(schema.files)
+              .set({
+                acknowledged: true,
+                acknowledgedTxHash: log.transactionHash,
+              })
+              .where(eq(schema.files.pieceCid, cid))
+              .run();
+          }
+
+          if (log.eventName === "SignatureSubmitted") {
+            const signatureData =
+              await contracts.FSFileRegistry.read.getSignatureData([
+                log.args.cidIdentifier,
+              ]);
+
+            const fileData = await contracts.FSFileRegistry.read.getFileData([
+              log.args.cidIdentifier,
+            ]);
+
+            const cid = concatHex([
+              fileData.pieceCidPrefix,
+              toHex(fileData.pieceCidTail),
+            ]);
+
+            const signatureExists = db
+              .select()
+              .from(schema.fileSignatures)
+              .where(
+                eq(schema.fileSignatures.onchainTxHash, log.transactionHash)
+              )
+              .get();
+
+            if (signatureExists) {
+              return;
+            }
+
+            db.insert(schema.fileSignatures)
+              .values({
+                filePieceCid: cid,
+                signerWallet: log.args.signer,
+                signatureVisualHash: signatureData.signatureVisualHash,
+                timestamp: signatureData.timestamp,
+                compactSignature: serializeCompactSignature(
+                  signatureToCompactSignature({
+                    v: BigInt(signatureData.v),
+                    r: signatureData.r,
+                    s: signatureData.s,
+                  })
+                ),
+                onchainTxHash: log.transactionHash,
+              })
+              .run();
+          }
+        }
       }
-    }
+    });
+  } finally {
+    dbC.$client.close();
   }
 }
