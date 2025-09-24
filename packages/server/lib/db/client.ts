272a132c28a80865a07f6bca654defb6e59bd551
Spandan Barve <spandan567@gmail.com>
2025-09-25 06:14:53 +0800

    no pooled dbClient

--- packages/server/lib/db/client.ts
+++ packages/server/lib/db/client.ts
@@ -4,12 +4,15 @@ import schema from "./schema";
 import { SQL } from "bun";
 import { sqliteFile } from "../../drizzle.config";
 
-const sqlite = new Database(sqliteFile);
-sqlite.run("PRAGMA foreign_keys = ON");
+function getSqliteClient() {
+  const sqlite = new Database(sqliteFile);
+  sqlite.run("PRAGMA foreign_keys = ON");
+  return sqlite;
+}
 
 export const createDbClient = () =>
   drizzle({
-    client: sqlite,
+    client: getSqliteClient(),
     schema: schema,
     casing: "snake_case",
   });
