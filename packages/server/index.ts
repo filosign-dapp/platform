9f616aac96224be9658c0778a2ff4dfa2781f7b8
Spandan Barve <spandan567@gmail.com>
2025-09-25 06:15:03 +0800

    rename main file

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,16 +3,18 @@ import { startIndexer } from "./lib/indexer/engine";
 import { startJobScheduler } from "./lib/indexer/scheduler";
 
 startIndexer("FSFileRegistry");
+startIndexer("FSKeyRegistry");
+startIndexer("FSManager");
 const workerId = `${require("os").hostname()}:${process.pid}`;
 startJobScheduler(workerId);
 
-const app = new Hono();
+// const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+// app.get("/", (c) => {
+//   return c.text("Hello Hono!");
+// });
 
-const port = 3000;
-console.log(`Server is running on port ${port}`);
+// const port = 3000;
+// console.log(`Server is running on port ${port}`);
 
-export default app;
+// export default app;
