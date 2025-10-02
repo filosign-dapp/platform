8e00d119f6fdc78ca0f0004934ea0302386c7773
Spandan Barve <spandan567@gmail.com>
2025-10-03 03:57:51 +0800

    synapse stuff

--- packages/server/lib/synapse/index.ts
+++ packages/server/lib/synapse/index.ts
@@ -1,8 +1,59 @@
 import { Synapse, RPC_URLS } from "@filoz/synapse-sdk";
 import env from "../../env";
+import db from "../db";
+import { eq } from "drizzle-orm";
+import type { Address } from "viem";
+import { tryCatch } from "../utils/tryCatch";
 
-const synapse = await Synapse.create({
+const WITH_CDN = false;
+
+export const synapse = await Synapse.create({
   privateKey: env.EVM_PRIVATE_KEY_SYNAPSE,
   rpcURL: RPC_URLS.calibration.websocket,
-  withCDN: true,
+  withCDN: WITH_CDN,
 });
+
+export async function getOrCreateUserDataset(walletAddress: Address) {
+  const existing = db
+    .select()
+    .from(db.schema.usersDatasets)
+    .where(eq(db.schema.usersDatasets.walletAddress, walletAddress))
+    .get();
+  if (existing) {
+    const ctx = await tryCatch(
+      synapse.storage.createContext({
+        dataSetId: existing.dataSetId,
+        providerAddress: existing.providerAddress,
+        metadata: { filosign_user: walletAddress },
+      })
+    );
+
+    if (ctx.error) {
+      throw new Error(
+        "Fail to create synapse context for existing user dataset"
+      );
+    }
+
+    return ctx.data;
+  }
+
+  const ctx = await tryCatch(
+    synapse.storage.createContext({
+      metadata: { filosign_user: walletAddress },
+    })
+  );
+
+  if (ctx.error) {
+    throw new Error("Fail to create synapse context for new user dataset");
+  }
+
+  db.insert(db.schema.usersDatasets)
+    .values({
+      walletAddress,
+      dataSetId: ctx.data.dataSetId,
+      providerAddress: ctx.data.provider.serviceProvider,
+    })
+    .run();
+
+  return ctx.data;
+}
---
+++ packages/server/synapse-pay.ts
@@ -0,0 +1,14 @@
+import { TOKENS, CONTRACT_ADDRESSES } from "@filoz/synapse-sdk";
+import { ethers } from "ethers";
+import { synapse } from "./lib/synapse";
+
+const amount = ethers.parseUnits("5", 18); // USDFC
+await synapse.payments.deposit(amount);
+
+const warmStorageAddress = synapse.getWarmStorageAddress();
+await synapse.payments.approveService(
+  warmStorageAddress,
+  /*rate allowance*/ ethers.parseUnits("10", 18),
+  /*lockup allowance*/ ethers.parseUnits("1000", 18),
+  /*max lockup preiod*/ 86_400n /* 30 days (in epochs) */
+);
