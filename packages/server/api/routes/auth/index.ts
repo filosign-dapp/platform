8a940ea25bf8d43411cd5f9ceec2da1bfb5cbe03
jriyyya <riya1jain567@gmail.com>
2025-10-03 22:04:05 +0530

    implement user profile management endpoints including retrieval, creation, and update; add username availability check

--- packages/server/api/routes/auth/index.ts
+++ packages/server/api/routes/auth/index.ts
@@ -10,8 +10,12 @@ import { primaryChain } from "../../../config";
 import { DOMAIN, URI } from "../../../constants";
 import { provider } from "../../../lib/indexer/provider";
 import { issueJwtToken } from "../../../lib/utils/jwt";
+import { authenticated } from "../../middleware/auth";
+import db from "../../../lib/db";
+import { eq } from "drizzle-orm";
 
 const messages: Record<Address, { message: string; validTill: number }> = {};
+const { users, profiles } = db.schema;
 
 export default new Hono()
 
@@ -71,61 +75,89 @@ export default new Hono()
 
     const token = issueJwtToken(wallet);
     return respond.ok(ctx, { valid, token }, "Signature verified", 200);
+  })
+
+  .get("/profile", authenticated, async (ctx) => {
+    const wallet = ctx.var.userWallet;
+
+    const userData = db
+      .select({
+        walletAddress: users.walletAddress,
+        email: users.email,
+        lastActiveAt: users.lastActiveAt,
+        createdAt: users.createdAt,
+        updatedAt: users.updatedAt,
+        username: profiles.username,
+        displayName: profiles.displayName,
+        avatarUrl: profiles.avatarUrl,
+        bio: profiles.bio,
+        metadataJson: profiles.metadataJson,
+      })
+      .from(users)
+      .leftJoin(profiles, eq(users.walletAddress, profiles.walletAddress))
+      .where(eq(users.walletAddress, wallet))
+      .get();
+
+    if (!userData) {
+      return respond.err(ctx, "User not found", 404);
+    }
+
+    return respond.ok(ctx, userData, "User data retrieved", 200);
+  })
+
+  .get("/profile/:username", authenticated, async (ctx) => {
+
+    const { username } = ctx.req.param();
+
+    const usernameAvailable = db.select().from(profiles).where(eq(profiles.username, username)).get();
+    if (usernameAvailable) {
+      return respond.ok(ctx, { available: false }, "Username unavailable", 200);
+    }
+    return respond.ok(ctx, { available: true }, "Username available", 200);
+  })
+
+  .post("/profile", authenticated, async (ctx) => {
+    const wallet = ctx.var.userWallet;
+    const { username, displayName } = await ctx.req.json();
+
+    const existingUsername = db.select().from(profiles).where(eq(profiles.username, username)).get();
+    if (existingUsername) {
+      return respond.err(ctx, "Username already exists", 400);
+    }
+
+    const newProfile = db.insert(profiles).values({
+      walletAddress: wallet,
+      username,
+      displayName
+    }).returning().get();
+
+    return respond.ok(ctx, newProfile, "Profile created successfully", 201);
+  })
+
+  .put("/profile", authenticated, async (ctx) => {
+    const wallet = ctx.var.userWallet;
+    const { username, displayName, avatarUrl, bio, metadataJson } = await ctx.req.json();
+
+    const existingProfile = db
+      .select()
+      .from(profiles)
+      .where(eq(profiles.walletAddress, wallet))
+      .get();
+
+    if (!existingProfile) {
+      return respond.err(ctx, "Profile not found", 404);
+    }
+
+    const updatedProfile = db.update(profiles)
+      .set({
+        username,
+        displayName,
+        avatarUrl,
+        bio,
+        metadataJson,
+      })
+      .where(eq(profiles.walletAddress, wallet)).returning()
+      .get();
+
+    return respond.ok(ctx, updatedProfile, "Profile updated successfully", 200);
   });
-
-//   .get("/claim/nonce", async (ctx) => {
-//     const wallet = ctx.req.query("wallet_address");
-//     if (!wallet || !isAddress(wallet)) {
-//       return respond.err(ctx, "Missing wallet address", 400);
-//     }
-//     const nonce = toHex(Uint8Array.from(Bun.randomUUIDv7()).slice(0, 20));
-//     const validTill = Date.now() + 30 * 1000; // 30 seconds
-//     nonces[wallet] = { nonce, validTill };
-//     return respond.ok(ctx, { nonce, validTill }, "Nonce generated", 200);
-//   })
-
-//   .post("/claim/verify", async (ctx) => {
-//     const { authSignature, walletSignature, walletAddress, authAddress } =
-//       await ctx.req.json();
-
-//     const nonceData = nonces[walletAddress];
-//     delete nonces[walletAddress];
-
-//     if (!nonceData || nonceData.validTill < Date.now()) {
-//       return respond.err(ctx, "Nonce expired or not found", 400);
-//     }
-//     if (!authSignature || !walletSignature) {
-//       return respond.err(ctx, "Missing signatures", 400);
-//     }
-
-//     const hashWithAuthAddress = keccak256(
-//       encodePacked(["bytes20", "address"], [nonceData.nonce, authAddress])
-//     );
-//     const hashWithWalletAddress = keccak256(
-//       encodePacked(["bytes20", "address"], [nonceData.nonce, walletAddress])
-//     );
-
-//     const validAuthSignature = await verifyMessage({
-//       address: authAddress,
-//       message: hashWithWalletAddress,
-//       signature: authSignature,
-//     });
-//     const validWalletSignature = await verifyMessage({
-//       address: walletAddress,
-//       message: hashWithAuthAddress,
-//       signature: walletSignature,
-//     });
-
-//     const valid = validAuthSignature && validWalletSignature;
-
-//     if (!valid) {
-//       return respond.err(ctx, "Invalid signatures", 400);
-//     }
-
-//     db.update(users)
-//       .set({ authAddress })
-//       .where(eq(users.walletAddress, walletAddress))
-//       .run();
-
-//     return respond.ok(ctx, { valid }, "Signatures verified", 200);
-//   });
